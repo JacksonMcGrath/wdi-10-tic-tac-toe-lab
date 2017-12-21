8c45b79813cc9145d853c3aaf83f3db631d8bfe4
JacksonMcgrath <jacksonfmcgrath@gmail.com>
2017-12-21 16:30:50 -0600

    added grid to board

--- Jackson_McGrath/js/app.js
+++ Jackson_McGrath/js/app.js
@@ -3,3 +3,14 @@ let $clearBoard = $('#clear-board')
 $clearBoard.on('click', () => {
 	console.log('clearBoard is firing');
 })
+
+let $board = $('#board')
+
+const generateSquares = () => {
+	for (i = 0; i < 9; i++) {
+		let $newSquare = $('<div>').addClass('square');
+		$board.append($newSquare);		
+	}
+}
+
+generateSquares();
